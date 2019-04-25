f6980621dd5aaa26e57b5aa5fd28834ceb410ca2
Luke Keller <39537709+Short-Hop@users.noreply.github.com>
2019-04-25 11:05:21 -0700

    Added API functionality to comments section

--- sprint-3/script/script.js
+++ sprint-3/script/script.js
@@ -1,31 +1,10 @@
-// Three Given Comment Objects (with dates converted to Milliseconds since Epoch)
-let comment1 = {
-    name: 'Micheal Lyons',
-    body: 'They BLEW the ROOF off at their last show, once everyone started figuring out they were going. This is still simply the greatest opening of a concert I have EVER witnessed.',
-    date: 1545120000000
-};
 
-let comment2 = {
-    name: 'Gary Wong',
-    body: 'Every time I see him shred I feel so motivated to get off my couch and hop on my board.He’s so talented! I wish I can ride like him one day so I can really enjoy myself!',
-    date: 1544601600000
-};
-
-let comment3 = {
-    name: 'Theodore Duncan',
-    body: 'How can someone be so good!!! You can tell he lives for this and lovesto do it every day. Everytime I see him I feel instantly happy! He’s definitely my favorite ever!',
-    date: 1542268800000
-}
-
-// Array of Comment Ojects
-let allComments = [comment1, comment2, comment3];
 
 
 // Function that appends HTML for an array of Comment Objects
 function printComments() {
-    for (let i = 0; i < allComments.length; i++) {
-        displayComment(allComments[i]);
-    }
+    allComments.forEach(displayComment);
+
 };
 
 // Function that removes all displayed comments
@@ -37,18 +16,22 @@ function clearComments() {
         toRemove = commentTop.nextElementSibling;
         main.removeChild(toRemove);
     }
+    allComments = [];
 }
 
 // Function that creates a comment Object and adds it to the Comment Object Array
-function addComment (name1, body1, date1, commentArray) {
+function makeComment (name1, comment1, timestamp1, id1, likes1) {
 
     let newComment = {
         name: name1,
-        body: body1,
-        date: date1,
+        id: id1,
+        likes: likes1,
+        comment: comment1,
+        timestamp: timestamp1,
     }
-    commentArray.unshift(newComment);
+    return newComment;
 };
+
 
 // Function that appends appropriate HTML using data from a Comment Object
 function displayComment(commentObject) {
@@ -62,10 +45,10 @@ function displayComment(commentObject) {
     let postDate = document.createElement('h5');
     postDate.classList.add('main__comments--date');
     // Date converted to easily-read Format
-    postDate.innerHTML = dateConvert(commentObject.date);
+    postDate.innerHTML = dateConvert(commentObject.timestamp);
 
     let postBody = document.createElement('h4');
-    postBody.innerHTML = commentObject.body;
+    postBody.innerHTML = commentObject.comment;
 
     let container = document.createElement('div');
     container.classList.add('main__comments--body');
@@ -89,8 +72,18 @@ function displayComment(commentObject) {
 // Converts a date from Milliseconds since Epoch to Easily-read Format
 function dateConvert(commentTime) {
 
-    let date = new Date();
-    let time = date.getTime();
+    //console.log('commented: ' + commentTime)
+
+    let time = Date.now();
+
+    //console.log('now: ' + time);
+
+    let difference = time - commentTime;
+
+    console.log(difference);
+    console.log(new Date(difference));
+
+
 
     time = Math.round(time / 1000);
     commentTime = Math.round(commentTime / 1000);
@@ -98,7 +91,12 @@ function dateConvert(commentTime) {
     let timeUnit = '';
     let timeNumber = 0;
 
-    if ((time - commentTime) < 60) {
+
+
+    if ((time-commentTime) < 0) {
+        return('Just now')
+    }
+    else if ((time - commentTime) < 60) {
         timeUnit = 'seconds';
         timeNumber = time - commentTime;
     }
@@ -134,49 +132,86 @@ function dateConvert(commentTime) {
     return(timeNumber + ' ' + timeUnit + ' ago');
 }
 
-// print existing Object Array comments
-printComments();
+// Gets all the comments in the API, stores them in the allComments array, and posts them to the page
+function getAPIComments() {
+    axios.get(commentURL).then(response => {
+        response.data.forEach(function (comment) {
+            allComments.unshift(makeComment(comment.name, comment.comment, comment.timestamp, comment.id, comment.likes));   
+        })
+        printComments();
+    });
+}
+const api_key = '?api_key=5864a759-f7e2-4f1c-8137-57936b5c4952'
+const commentURL = 'https://project-1-api.herokuapp.com/comments?api_key=5864a759-f7e2-4f1c-8137-57936b5c4952'
+const showdatesURL = 'https://project-1-api.herokuapp.com/showdates?api_key=5864a759-f7e2-4f1c-8137-57936b5c4952'
 
-// Event when Comment Form is submitted.  
-const commentForm = document.getElementById('commentForm');
-document.getElementById('commentForm').addEventListener('submit', function (event) {    
+// sends a comment to the API, clears the current comments, and updates the allComments array
+function postComment (comment) {
+
+    axios.post(commentURL, comment)
+    .then(function (response) {
+        console.log(response);
+        clearComments();
+
+        getAPIComments();
+    })
+
     
+}
+
+// Removes the comment from the API, clears the displayed comments, and gets the remaining comments
+function deleteComment (id) {
+    let deleteURL = 'https://project-1-api.herokuapp.com/comments/' + id + api_key;
+    axios.delete(deleteURL).then(response => {
+        console.log(response);
+
+        clearComments();
+
+        getAPIComments();
+    })
+    
+}
+
+
+
+
+// Array of Comment Ojects
+let allComments = [];
+
+// print existing Object Array comments
+getAPIComments();
+
+
+
+
+
+
+
+//Waits for the form to be submitted, then posts the comment
+const commentForm = document.getElementById('commentForm');
+document.getElementById('commentForm').addEventListener('submit', function (event) {
+
     // Submitted Data is stored in variables in the correct format
     event.preventDefault();
 
-    let name1 = event.target.fullName.value;
+    let newComment = {
+        name: event.target.fullName.value,
+        comment: event.target.comment.value,
 
-    let body1 = event.target.comment.value;
+    }
 
-    // Old code for old date format
-
-    // let date = new Date();
-    // console.log(date);
-    // let day = date.getDate();
-    // let month = date.getMonth() + 1;
-    // let year = date.getFullYear();
-    // let date1 = month + '/' + day + '/' + year;
-
-
-    let date = new Date();
-    let date1 = date.getTime();
-
-    // All previous comments are cleared
-    clearComments();
-
-    // Submitted Comment is placed into a Comment Object at the front of the Comment Object Array
-    addComment(name1, body1, date1, allComments);
-
-    // Comment Object Array is appended as HTML
-    printComments();
+    // Submitted Comment is placed into a Comment Object and Sent to the API, Comment section is updated
+    postComment(newComment);
 
     // Form text boxes are emptied
     let textBoxes = document.querySelectorAll('.main__comments--input');
 
     for (let i = 0; i < textBoxes.length; i++) {
-        textBoxes[i].value = '';  
-    }  
+        textBoxes[i].value = '';
+    }
 });
+
+
 
 
 
